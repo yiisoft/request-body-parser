d4d93dc0fe07b869aa5d55f52a8854218323a38b
Alexander Makarov <sam@rmcreative.ru>
2020-07-18 16:43:07 +0300

    Use ParserException message in BadRequestHandler

--- src/BadRequestHandler.php
+++ src/BadRequestHandler.php
@@ -13,16 +13,18 @@ use Yiisoft\Http\Status;
 final class BadRequestHandler implements RequestHandlerInterface
 {
     private ResponseFactoryInterface $responseFactory;
+    private ParserException $parserException;
 
-    public function __construct(ResponseFactoryInterface $responseFactory)
+    public function __construct(ResponseFactoryInterface $responseFactory, ParserException $parserException)
     {
         $this->responseFactory = $responseFactory;
+        $this->parserException = $parserException;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $response = $this->responseFactory->createResponse(Status::BAD_REQUEST);
-        $response->getBody()->write(Status::TEXTS[Status::BAD_REQUEST]);
+        $response->getBody()->write(Status::TEXTS[Status::BAD_REQUEST] . "\n" . $this->parserException->getMessage());
         return $response;
     }
 }
