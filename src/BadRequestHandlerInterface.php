db7fc5d2e0c5ca4ead18e1a4da05c261154379f5
Alexander Makarov <sam@rmcreative.ru>
2020-07-18 17:08:55 +0300

    Cleanup

--- src/BadRequestHandlerInterface.php
+++ src/BadRequestHandlerInterface.php
@@ -1,15 +1,10 @@
 <?php
 
-
 namespace Yiisoft\Request\Body;
 
-
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 interface BadRequestHandlerInterface extends RequestHandlerInterface
 {
-    public function handle(ServerRequestInterface $request): ResponseInterface;
     public function withParserException(ParserException $e): BadRequestHandlerInterface;
 }
